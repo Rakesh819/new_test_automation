3c52f7a6e91f6f3dd47ce39d478bdcbe508d23c9
Rakesh819 <120001272+Rakesh819@users.noreply.github.com>
2024-09-24 09:40:48 +0530

    Add files via upload

---
+++ test_automation_page2/src/main/java/test_automation_page2/test_automation_page2/App.java
@@ -0,0 +1,13 @@
+package test_automation_page2.test_automation_page2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
